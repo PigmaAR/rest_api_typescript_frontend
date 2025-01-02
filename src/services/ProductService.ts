e88f4ea1efc276d69c1214943eb78d0d2e55d67a
RamunnoAJ <ramunnoagustin@gmail.com>
2025-01-02 11:17:09 -0300

    feat: edit product

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,6 +1,12 @@
-import { safeParse } from "valibot";
-import { DraftProductSchema, ProductSchema, ProductsSchema } from "../types";
+import { safeParse, transform, pipe, parse, string } from "valibot";
+import {
+  DraftProductSchema,
+  Product,
+  ProductSchema,
+  ProductsSchema,
+} from "../types";
 import axios from "axios";
+import { toBoolean } from "../utils";
 
 type ProductData = {
   [k: string]: FormDataEntryValue;
@@ -36,6 +42,40 @@ export async function getProducts() {
     } else {
       throw new Error("Hubo un error");
     }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function getProductByID(id: Product["id"]) {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+    const { data } = await axios(url);
+    const result = safeParse(ProductSchema, data.data);
+    if (result.success) {
+      return result.output;
+    } else {
+      throw new Error("Hubo un error");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function updateProduct(data: ProductData, id: Product["id"]) {
+  try {
+    const NumberSchema = pipe(
+      string(),
+      transform((input) => Number(input)),
+    );
+    const result = safeParse(ProductSchema, {
+      id,
+      name: data.name,
+      price: parse(NumberSchema, data.price),
+      availability: toBoolean(data.availability.toString()),
+    });
+
+    console.log(result);
   } catch (e) {
     console.log(e);
   }
